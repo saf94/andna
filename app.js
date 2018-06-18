367b0bfd015ae7924ae3c68bf15211f6acae5612
Safeer Hayat <shayat@sadeers-mbp.home>
2018-06-18 20:05:27 +0100

    added mongo code back in

--- app.js
+++ app.js
@@ -23,13 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// router.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/public/index.html'));
-// })
-// router.get('/bio', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/public/bio.html'));
-// })
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -45,5 +38,21 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("appdb");
+  dbo.createCollection("profiles", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+  });
+  // dbo.collection("profiles").insertOne({ username: "shayat@and.digital", name: "Saf Hayat"}, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("1 document inserted");
+    db.close();    
+  // });
+}); 
 
 module.exports = app;
