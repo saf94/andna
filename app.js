f12561240ce8a516fb47d8a117a77fbcfdbba8f6
Kris Rogowski <krogowski@MacBook-Pro-6.local>
2018-06-18 16:33:05 +0100

    here comes the big thing

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-/*
-const http = require('http');
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.use(express.static('public'));
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-var router = express.Router();
+var app = express();
 
-router.get('/', function(req, res) {
-  res.send('index.html')
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-router.get('/bio', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/bio.html'));
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', router);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.set("view engine", "html");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("appdb");
-  dbo.createCollection("profiles", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-  });
-
-  dbo.collection("profiles").insertOne({ username: "shayat@and.digital", name: "Saf Hayat"}, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();    
-  });
-}); 
-
-*/
+module.exports = app;
