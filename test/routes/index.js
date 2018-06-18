4ad666e9c619b6e88824868adc95f24202256885
Kris Rogowski <krogowski@MacBook-Pro-6.local>
2018-06-18 15:43:32 +0100

    added GET picture

--- test/routes/index.js
+++ test/routes/index.js
@@ -42,3 +42,24 @@ db.collection('profiles')
    });
    };
 });
+
+router.get('/picture/:picture', function(req, res){
+// assign the URL parameter to a variable
+var filename = req.params.picture;
+// open the mongodb connection with the connection
+// string stored in the variable called url.
+   MongoClient.connect(url, function(err, db){
+   db.collection('profiles')
+// perform a mongodb search and return only one result.
+// convert the variabvle called filename into a valid
+// objectId.
+     .findOne({'_id': ObjectId(filename)}, function(err, results){
+// set the http response header so the browser knows this
+// is an 'image/jpeg' or 'image/png'
+    res.setHeader('content-type', results.contentType);
+// send only the base64 string stored in the img object
+// buffer element
+         res.send(results.img.buffer);
+      });
+   });
+});
