e671be65aca41558c37914abff7d81357e8a98fb
Safeer Hayat <shayat@sadeers-mbp.home>
2018-06-18 19:50:19 +0100

    setting up routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// app.set("view engine", "html");
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// router.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/public/index.html'));
+// })
+// router.get('/bio', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/public/bio.html'));
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
