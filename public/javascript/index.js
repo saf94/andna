f94692e445c4ff3018b4609102b0e35c7e8731ea
Safeer Hayat <shayat@Sadeers-MacBook-Pro.local>
2018-06-19 16:56:04 +0100

    tidying up styling

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -20,7 +20,7 @@ function removeElement(id) {
 
 document.getElementById("addExp").addEventListener("click", function() {
     if(document.getElementsByClassName("sectionBackground").length < 4)
-        document.getElementById("newExperience").innerHTML += '<section class="sectionBackground" id="experience' + expId + '"><h3>Experiences:</h3><div class="form-group"><label for="email">Role:</label><input type="role" class="form-control" id="role" placeholder="Scrum master/PA/PD" name="role"></div><div class="form-group"><label for="title">Location</label><input type="location" class="form-control" id="location" placeholder="E.g. Avios/Halfords/BA" name="location"></div><div class="form-group"><label for="LinkedIn">Summary</label><input type="roleSummary" class="form-control" id="roleSummary" placeholder="Summary of responsibilities" name="roleSummary"><button type="button" class="btn btn-danger" id="deleteExp" onclick="removeElement(' + expId + ')"> - </button></section>'
+        document.getElementById("newExperience").innerHTML += '<section class="sectionBackground" id="experience' + expId + '"><h3>Experiences:</h3><div class="form-group"><label for="email">Role:</label><input type="role" class="form-control" id="role" placeholder="Scrum master/PA/PD" name="role"></div><div class="form-group"><label for="title">Location</label><input type="location" class="form-control" id="location" placeholder="E.g. Avios/Halfords/BA" name="location"></div><div class="form-group"><label for="LinkedIn">Summary</label><input type="roleSummary" class="form-control" id="roleSummary" placeholder="Summary of responsibilities" name="roleSummary"><button type="button" id="deleteExp" onclick="removeElement(' + expId + ')"> - </button></section>'
         expId++;
     })
 
