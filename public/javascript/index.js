7f60f60b7c5b4df956e6ed4783ba450e7812d71e
Safeer Hayat <shayat@Sadeers-MacBook-Pro.local>
2018-06-19 15:03:10 +0100

    building out form

---
+++ public/javascript/index.js
@@ -0,0 +1,16 @@
+// console.log("hello world!");
+
+// // var newExp = document.getElementById("experience").cloneNode(true);
+// // // var addButton = document.getElementById("addExp");
+
+// // document.getElementById("addExp").addEventListener("click", function() {
+// //     document.getElementById("newExperience").appendChild(newExp);
+// //     // document.getElementById("newExperience").appendChild(addButton);
+
+// // })
+
+// var newExp = document.getElementById("experience");
+
+// document.getElementById("addExp").addEventListener("click", function() {
+//         document.getElementById("newExperience").innerHTML('<section class="sectionBackground" id="experience"><h3>Experiences:</h3><div class="form-group"><label for="email">Role:</label><input type="role" class="form-control" id="role" placeholder="Scrum master/PA/PD" name="role"></div><div class="form-group"><label for="title">Location</label><input type="location" class="form-control" id="location" placeholder="E.g. Avios/Halfords/BA" name="location"></div><div class="form-group"><label for="LinkedIn">Summary</label><input type="roleSummary" class="form-control" id="roleSummary" placeholder="Summary of responsibilities" name="roleSummary"></section>')
+// })
