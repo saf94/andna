59a43b9c90de518db16e9a853772cbda9cd6542c
Sophie Anzarouth <sanzarouth@and.digital>
2018-06-22 12:13:01 +0100

    Validation x2

--- public/javascript/forms.js
+++ public/javascript/forms.js
@@ -34,13 +34,37 @@ function removeTool(id) {
 }
 
 document.getElementById("addExp").addEventListener("click", function() {
-  if (document.getElementsByClassName("sectionBackground").length < 6) {
+  if (document.getElementsByClassName("sectionBackground").length < 4) {
     document.getElementById("newExperience").innerHTML +=
       '<section class="sectionBackground" id="experience' +
       expId +
-      '"><h3>Experiences:</h3><div class="form-group"><label for="email">Role:</label><input type="role" class="form-control" id="role" placeholder="Scrum master/PA/PD" name="role"></div><div class="form-group"><label for="title">Location</label><input type="location" class="form-control" id="location" placeholder="E.g. Avios/Halfords/BA" name="location"></div><div class="form-group"><label for="LinkedIn">Summary</label><input type="roleSummary" class="form-control" id="roleSummary" placeholder="Summary of responsibilities" name="roleSummary"><button type="button" id="deleteExp" onclick="removeElement(' +
+      '"><h3>Experiences:</h3><div class="form-group"><label for="email">Role:</label><input type="role" class="form-control" id="role' +
+      expId +
+      '" placeholder="Scrum master/PA/PD" name="role"></div><div class="form-group"><label for="title">Location</label><input type="location" class="form-control" id="location' +
+      expId +
+      '" placeholder="E.g. Avios/Halfords/BA" name="location"></div><div class="form-group"><label for="LinkedIn">Summary</label><input type="roleSummary" class="form-control" id="roleSummary' +
+      expId +
+      '" placeholder="Summary of responsibilities" name="roleSummary"><button type="button" id="deleteExp" onclick="removeElement(' +
       expId +
       ')"> - </button></section>';
+    document.documentElement.innerHTML +=
+      '<script>validate(document.getElementById("roleSummary' +
+      expId +
+      '"), document.getElementById("sumNum' +
+      expId +
+      '"), 300);</script>';
+    document.documentElement.innerHTML +=
+      '<script>validate(document.getElementById("role' +
+      expId +
+      '"), document.getElementById("roleNum' +
+      expId +
+      '"), 35);</script>';
+    document.documentElement.innerHTML +=
+      '<script>validate(document.getElementById("location' +
+      expId +
+      '"), document.getElementById("locationNum' +
+      expId +
+      '"), 25);</script>';
     expId++;
   }
 });
@@ -215,4 +239,16 @@ function autocomplete(inp, arr) {
   document.addEventListener("click", function(e) {
     closeAllLists(e.target);
   });
+}
+
+function validate(val, fieldId, limit) {
+  val.addEventListener("keyup", function(e) {
+    validateChar(val, fieldId, limit);
+  });
+
+  function validateChar(val, fieldId, limit) {
+    var text_length = val.value.length;
+    var text_remaining = limit - text_length;
+    $(fieldId).html(text_remaining + " characters remaining");
+  }
 }
